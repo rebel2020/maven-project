5d3fcf2a7c8fea4589f7959c9b28a092ee761562
brijesh <brijesh.dhakar.cse16@itbhu.ac.in>
2020-05-26 16:29:28 +0530

    configured junit tests

--- thrillio/test/com/thrillio/entities/BookTest.java
+++ thrillio/test/com/thrillio/entities/BookTest.java
@@ -1,26 +1,28 @@
 package com.thrillio.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
 
 import com.thrillio.constants.BookGenre;
 import com.thrillio.managers.BookmarkManager;
 
 import static org.junit.Assert.*;
-class BookTest {
+
+import org.junit.Test;
+public class BookTest {
 
 	@Test
-	void testIsKidFriendkyEligible() {
-		//Test 1- Genre is Philosophy- false
+	public void testIsKidFriendkyEligibleGenreIsPholosophy() {
+		//BookGenre is PHYLOSOPHY
 		Book book = BookmarkManager.getInstance().createBook(4000,"Walden","",1854,"Wilder Publications",new String[] {"Henry David Thoreau"},BookGenre.PHILOSOPHY,4.3);
-		boolean isKidFriendkyEligible = book.isKidFriendkyEligible();
-		assertFalse("Genre is Philosophy- must return false",isKidFriendkyEligible);
-		
-		//Test 2- Genre is Self help- false
-		book = BookmarkManager.getInstance().createBook(4000,"Walden","",1854,"Wilder Publications",new String[] {"Henry David Thoreau"},BookGenre.SELF_HELP,4.3);
-		isKidFriendkyEligible = book.isKidFriendkyEligible();
-		assertFalse("Genre is Self help- must return false",isKidFriendkyEligible);
+		Boolean isKidFriendlyEligible = book.isKidFriendkyEligible();
+		assertFalse("Genre is Philosophy- must return false",isKidFriendlyEligible);
 	}
+	@Test
+	public void testIsKidFriendkyEligibleGenreIsSelfHelp() {
+		//BookGenre is SELF_HELP
+		Book book = BookmarkManager.getInstance().createBook(4000,"Walden","",1854,"Wilder Publications",new String[] {"Henry David Thoreau"},BookGenre.SELF_HELP,4.3);
+		Boolean isKidFriendlyEligible = book.isKidFriendkyEligible();
+		assertFalse("Genre is Selp help- must return false",isKidFriendlyEligible);
+	}
+
 
 }
--- thrillio/test/com/thrillio/entities/MovieTest.java
+++ thrillio/test/com/thrillio/entities/MovieTest.java
@@ -1,25 +1,28 @@
 package com.thrillio.entities;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 
 import com.thrillio.constants.MovieGenre;
 import com.thrillio.managers.BookmarkManager;
 
-class MovieTest {
+public class MovieTest {
 
-	@Test
-	void testIsKidFriendkyEligible() {
+	@org.junit.Test
+	public void testIsKidFriendkyEligibleGenreIsHorror() {
 		// Test 1- Genre is Horror- false
 		Movie movie = BookmarkManager.getInstance().createMovie(3000,"Citizen Kane","",1941,new String[] {"Orson Welles","Joseph Cotten"},new String[] {"Orson Welles"},MovieGenre.HORROR,8.5);
 		boolean isKidFriendkyEligible = movie.isKidFriendkyEligible();
 		assertFalse("Genre is Horror- must return false",isKidFriendkyEligible);
 		
-		// Test 2- Genre is Thriller- false
-		movie = BookmarkManager.getInstance().createMovie(3000,"Citizen Kane","",1941,new String[] {"Orson Welles","Joseph Cotten"},new String[] {"Orson Welles"},MovieGenre.THRILLER,8.5);
-		isKidFriendkyEligible = movie.isKidFriendkyEligible();
+	}
+	@org.junit.Test
+	public void testIsKidFriendkyEligibleGenreIsThriller() {
+		// Test- Genre is Thriller- false
+		Movie movie= BookmarkManager.getInstance().createMovie(3000,"Citizen Kane","",1941,new String[] {"Orson Welles","Joseph Cotten"},new String[] {"Orson Welles"},MovieGenre.THRILLER,8.5);
+		boolean isKidFriendkyEligible = movie.isKidFriendkyEligible();
 		assertFalse("Genre is Thriller- must return false",isKidFriendkyEligible);
 	}
 
